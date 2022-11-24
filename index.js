d55245d2c720dcff54ec19f75e8e51e15dc26e04
+ <+>
2022-11-24 18:37:46 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+//let a = 2000
+//let b = 100
+
+const divide = (a , b) =>{
+    return 2000 /100
+
+}
+//let x = 2
+const  square = (x) =>{
+ 
+    return x * x;
+}
+//let a =3
+//let b =4
+const add = (a ,b) => {
+    return a + b;
+
+}
